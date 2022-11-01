a3cde6019c0b26a9db76837676f0e459c84357d4
Cheol-Cheol <dream174822@gmail.com>
2022-11-01 15:19:20 +0900

    Revert "✨ Bottom Tabs Navigation 구현"
    This reverts commit 14ea94bb708980ce9cd75ff282ebbda37efc8cce.

--- screen/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-const HomePage = () => {
-  return <Text>HomePage</Text>;
-};
-
-export default HomePage;
--- screen/UserPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-const UserPage = () => {
-  return <Text>UserPage</Text>;
-};
-
-export default UserPage;
