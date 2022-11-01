82aa876ff616de7dc363f722e983f4a1c6e8de33
Cheol-Cheol <dream174822@gmail.com>
2022-11-01 16:39:53 +0900

    ✨ Dark Mode 구현

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+} from "@react-navigation/native";
 import Navigator from "./navigation/Navigator";
+import { ThemeProvider } from "styled-components/native";
+import { darkTheme, lightTheme } from "./theme";
+import { useColorScheme } from "react-native";
 
 export default function App() {
+  const isDark = useColorScheme() === "dark";
+
   return (
-    <NavigationContainer>
-      <Navigator />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+        <Navigator />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
---
+++ theme.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  bgColor: "white",
+  textColor: "",
+  btnColor: "",
+};
+
+export const darkTheme = {
+  bgColor: "black",
+  textColor: "",
+  btnColor: "",
+};
