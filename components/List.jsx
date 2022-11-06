d29c17ef0728c227365831d6ff23fbb4de0340c4
Cheol-Cheol <dream174822@gmail.com>
2022-11-06 19:03:25 +0900

    ✨ Modal component 추가

--- components/List.jsx
+++ components/List.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { Dimensions } from "react-native";
+import React, { useState } from "react";
+import { Dimensions, Modal, View, Alert, Text, Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
 
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const ListContainer = styled.TouchableOpacity`
   background-color: white;
@@ -40,21 +41,100 @@ const TextRight = styled.Text`
   font-weight: 700;
 `;
 
-const List = ({ fullData }) => {
-  return (
-    <ListContainer>
-      <ListLogo>
-        <Ionicons name="person" size={45} />
-      </ListLogo>
+const ModalContainer = styled.View``;
 
-      <ListContent style={{ width: SCREEN_WIDTH / 1.5 }}>
-        <Title>{fullData.title}</Title>
-        <RowView>
-          <TextLeft>{fullData.businessName}</TextLeft>
-          <TextRight>{fullData.orderTime.substring(14)}</TextRight>
-        </RowView>
-      </ListContent>
-    </ListContainer>
+const ModalTitle = styled.Text`
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+const ModalBtns = styled.View`
+  width: 220px;
+  margin-top: 50px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Btn = styled.Pressable`
+  background-color: ${({ color }) => color};
+  width: 100px;
+  padding: 15px 20px;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BtnText = styled.Text`
+  color: white;
+  font-size: 15px;
+`;
+
+const List = ({ fullData }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const navigation = useNavigation();
+
+  const goToChatPage = () => {
+    setModalVisible(!modalVisible);
+    navigation.navigate("ChatPage", {
+      params: { ...fullData },
+    });
+  };
+
+  return (
+    <>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <ModalContainer
+          style={{
+            backgroundColor: "white",
+            margin: 25,
+            marginTop: SCREEN_HEIGHT / 2.8,
+            padding: 50,
+            borderRadius: 20,
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5,
+          }}
+        >
+          <ModalTitle>채팅방에 참가하시겠습니까?</ModalTitle>
+          <ModalBtns>
+            <Btn color={"grey"} onPress={() => setModalVisible(!modalVisible)}>
+              <BtnText>취소</BtnText>
+            </Btn>
+            <Btn color={"#3772ff"} onPress={goToChatPage}>
+              <BtnText>확인</BtnText>
+            </Btn>
+          </ModalBtns>
+        </ModalContainer>
+      </Modal>
+
+      <ListContainer onPress={() => setModalVisible(!modalVisible)}>
+        <ListLogo>
+          <Ionicons name="person" size={45} />
+        </ListLogo>
+
+        <ListContent style={{ width: SCREEN_WIDTH / 1.5 }}>
+          <Title>{fullData.title}</Title>
+          <RowView>
+            <TextLeft>{fullData.businessName}</TextLeft>
+            <TextRight>{fullData.orderTime.substring(14)}</TextRight>
+          </RowView>
+        </ListContent>
+      </ListContainer>
+    </>
   );
 };
 
