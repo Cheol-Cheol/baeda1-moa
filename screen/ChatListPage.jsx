21a4e4bc7e4dfab5b1792c90b6a1facc8c9ce1a5
Cheol-Cheol <dream174822@gmail.com>
2022-11-08 23:11:24 +0900

    💄 ChatListPage UI 생성

--- screen/ChatListPage.jsx
+++ screen/ChatListPage.jsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
+import ChatRoom from "../components/ChatRoom";
+
+const DUMMY_DATA = [
+  {
+    roomId: "1",
+    admin: "1de1dxz01011",
+    title: "밥 먹자 애들아!",
+    dialog: "지금 시키죠ㅋㅋ",
+    time: "오후 8:12",
+  },
+  {
+    roomId: "2",
+    admin: "1de1dxz01012",
+    title: "족발 땡긴다!",
+    dialog: "2인분 더 시켜도 될까요?",
+    time: "오후 10:30",
+  },
+  {
+    roomId: "3",
+    admin: "1de1dxz01013",
+    title: "1/N 개이득!",
+    dialog: "돈 개많이 아꼈네~",
+    time: "오후 3:00",
+  },
+];
 
 const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
+  padding: 0px 15px;
+  padding-top: 10px;
   background-color: ${({ theme }) => theme.bgColor};
 `;
 
-const Text = styled.Text`
-  color: ${({ theme }) => theme.textColor};
+const FlatList = styled.FlatList``;
+
+const HSeparator = styled.View`
+  height: 5px;
 `;
 
 const ChatListPage = () => {
   return (
     <Container>
-      <Text>ChatListPage</Text>
+      <FlatList
+        data={DUMMY_DATA}
+        ItemSeparatorComponent={HSeparator}
+        keyExtractor={(item) => item.roomId}
+        renderItem={({ item }) => <ChatRoom fullData={item} />}
+      />
     </Container>
   );
 };
