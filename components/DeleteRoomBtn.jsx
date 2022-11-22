5d86d1deac285e4888c0eaa19b971db4c2b06226
Cheol-Cheol <dream174822@gmail.com>
2022-11-22 17:08:35 +0900

    ✨ DeleteRoomBtn 컴포넌트 생성

---
+++ components/DeleteRoomBtn.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
+
+const BtnContainer = styled.View`
+  flex-direction: row;
+`;
+
+const Btn = styled.TouchableOpacity`
+  margin: 2px;
+`;
+
+const DeleteRoomBtn = ({ roomId, deleteRoomGoToChatListPage }) => {
+  return (
+    <>
+      <BtnContainer>
+        <Btn>
+          <Feather name="edit" size={22} color="black" />
+        </Btn>
+
+        <Btn onPress={() => deleteRoomGoToChatListPage(roomId)}>
+          <Feather name="log-out" size={22} color="black" />
+        </Btn>
+      </BtnContainer>
+    </>
+  );
+};
+
+export default DeleteRoomBtn;
